e2ade00af55aa9c203ae667a90c1aaffae93bc06
Matias Garcia <matias.garcia@tiendanube.com>
2024-02-25 21:26:51 -0300

    implement persistence

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlarmCommand } from './commands/create-alarm.command';
+import { AlarmRepository } from './ports/alarm.repository';
+import { AlarmFactory } from '../domain/factories/alarm-factory';
 
 @Injectable()
 export class AlarmsService {
-  create(CreateAlarmCommand: CreateAlarmCommand) {
-    return 'This action adds a new alarm';
+  constructor(
+    private readonly alarmRepository: AlarmRepository,
+    private readonly alarmFactory: AlarmFactory,
+  ) {}
+
+  create(createAlarmCommand: CreateAlarmCommand) {
+    const alert = this.alarmFactory.create(
+      createAlarmCommand.name,
+      createAlarmCommand.readonly,
+    );
+    return this.alarmRepository.save(alert);
   }
 
   findAll() {
-    return `This action returns all alarms`;
+    return this.alarmRepository.findAll();
   }
 }
