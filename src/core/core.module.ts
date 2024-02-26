29451df48f2cac7287df854ddd5fc9fd4d356a2a
Matias Garcia <matias.garcia@tiendanube.com>
2024-02-26 17:33:37 -0300

    configure mongoose on nestjs

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ApplicationBootstrapOptions } from '../common/interfaces/application-bootstrap-options.interface';
 
 @Module({})
@@ -19,6 +20,7 @@ export class CoreModule {
               autoLoadEntities: true,
               synchronize: true,
             }),
+            MongooseModule.forRoot('mongodb://localhost:27017/vf-read-db'),
           ]
         : [];
 
