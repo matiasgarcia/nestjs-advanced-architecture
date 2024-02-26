461d4444d43490e3e3455a9d5d316bcd78e27ffb
Matias Garcia <matias.garcia@tiendanube.com>
2024-02-26 09:54:19 -0300

    feat: create alarm when executing handler

--- src/alarms/application/commands/create-alarm.command-handler.ts
+++ src/alarms/application/commands/create-alarm.command-handler.ts
@@ -1,6 +1,8 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-alarm.command';
 import { Logger } from '@nestjs/common';
+import { AlarmRepository } from '../ports/alarm.repository';
+import { AlarmFactory } from '../../domain/factories/alarm-factory';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler
@@ -8,9 +10,16 @@ export class CreateAlarmCommandHandler
 {
   private readonly logger = new Logger(CreateAlarmCommandHandler.name);
 
+  constructor(
+    private readonly alarmRepository: AlarmRepository,
+    private readonly alarmFactory: AlarmFactory,
+  ) {}
+
   async execute(command: CreateAlarmCommand) {
     this.logger.debug(
       `Processing "CreateAlarmCommand": ${JSON.stringify(command)}`,
     );
+    const alarm = this.alarmFactory.create(command.name, command.severity);
+    return this.alarmRepository.save(alarm);
   }
 }
--- src/alarms/application/commands/create-alarm.command.ts
+++ src/alarms/application/commands/create-alarm.command.ts
@@ -1,3 +1,3 @@
 export class CreateAlarmCommand {
-  constructor(public readonly name: string, public readonly readonly: string) {}
+  constructor(public readonly name: string, public readonly severity: string) {}
 }
