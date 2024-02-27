0d83e7d21a888b2350076d02d7cdc67e764242c9
Matias Garcia <matias.garcia@tiendanube.com>
2024-02-27 10:09:24 -0300

    feat: autowire events

--- src/alarms/domain/events/alarm-created.event.ts
+++ src/alarms/domain/events/alarm-created.event.ts
@@ -1,5 +1,7 @@
-import { Alarm } from "../alarm";
+import { AutowiredEvent } from 'src/shared/decorators/autowired-event.decorator';
+import { Alarm } from '../alarm';
 
+@AutowiredEvent
 export class AlarmCreatedEvent {
   constructor(public readonly alarm: Alarm) {}
 }
