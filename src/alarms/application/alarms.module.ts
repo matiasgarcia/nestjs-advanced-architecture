3fc3e5c1809b5fbb0ad1216e4fb7a814fb41ffac
Matias Garcia <matias.garcia@tiendanube.com>
2024-03-02 10:37:27 -0300

    feat: add saga to notify if more than 3 alerts come in 5s

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -7,6 +7,7 @@ import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
 import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -18,6 +19,7 @@ import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledg
     AlarmCreatedEventHandler,
     AcknowledgeAlarmCommandHandler,
     AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
   ],
 })
 export class AlarmsModule {
